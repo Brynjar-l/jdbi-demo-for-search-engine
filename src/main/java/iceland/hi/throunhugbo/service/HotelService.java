3ab3bbb2cce751b66679a4e4e37860a34fbca000
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-02-14 09:59:41 +0000

    + HotelService.java, empty

---
+++ src/main/java/iceland/hi/throunhugbo/service/HotelService.java
@@ -0,0 +1,26 @@
+package iceland.hi.throunhugbo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelService {
+
+    // private List<String> whereClauses = new ArrayList<>();
+
+    /*
+     * getAll
+     * getName, bounds?
+     * getLocation
+     * getRating, bounds?
+     */
+
+
+
+    /*
+     * create a StringBuilder with base query
+     * maintain an ArrayList with additional Where clauses (Filters)
+     * .execute() method => return finalized query ; does not affect the ArrayList?
+     */
+
+
+}
